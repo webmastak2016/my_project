df1747c57232b47d44ad7d2953d01e2e1a77f39e
Denis G <webmastak2016@gmail.com>
2023-03-01 16:42:07 +0100

    php+js_test

--- PHP/index.php
+++ PHP/index.php
@@ -1,3 +1,13 @@
 <?php
-    111
+    function getSum($x, $y)
+    {
+        return $x + $y;
+    }
+    
+    $a = 5;
+    $b = 12;
+    
+    echo getSum($a, $b) . '<br>';
+    echo getSum(13, 4);
+    
 ?>
